941e7c18e003734aac0635d838bd39549ad19289
James <code-for-coffee@users.noreply.github.com>
2016-01-18 21:34:27 -0600

    Create app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+// define an ES6 class called HelloWorld
+class HelloWorld {
+  // define a toString() method on the class
+  toString() {
+    return 'Hello, world!';
+  }
+}
+// instantiate a new instance of HelloWorld
+var sample = new HelloWorld();
+// console.log sample's toString() method
+console.log(sample.toString());
