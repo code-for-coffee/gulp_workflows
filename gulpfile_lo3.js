9232f9871bf4acdbfb59d00f08115c4b4672e4d4
James <code-for-coffee@users.noreply.github.com>
2016-01-18 20:52:17 -0600

    Create gulpfile_lo3.js

---
+++ gulpfile_lo3.js
@@ -0,0 +1,7 @@
+var gulp = require('gulp');
+
+//define a task with the name of 'default' 
+// and a callback to perform when the task is ran
+gulp.task('default', function() {
+  console.log('I am the default task. Hear me roar');
+});
